11d41b126e1c4e28d6eee88c0f60d76673e56a30
kaizer <mir.rayan.iub@gmail.com>
2016-11-21 14:56:27 +0600

    created activity_signup.xml

---
+++ app/src/main/java/SignupActivity.java
@@ -0,0 +1,19 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import dsd.informme.com.helpblox.R;
+
+/**
+ * Created by kaizer on 11/21/16.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+}
