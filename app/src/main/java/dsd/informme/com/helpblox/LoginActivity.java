a8209d37c926726b5ff300ce1494927e263585c3
kaizer <mir.rayan.iub@gmail.com>
2016-11-21 18:27:23 +0600

    LoginActivity and SignupActivity created

---
+++ app/src/main/java/dsd/informme/com/helpblox/LoginActivity.java
@@ -0,0 +1,17 @@
+package dsd.informme.com.helpblox;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kaizer on 11/21/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/dsd/informme/com/helpblox/SignupActivity.java
+++ app/src/main/java/dsd/informme/com/helpblox/SignupActivity.java
@@ -1,3 +1,5 @@
+package dsd.informme.com.helpblox;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
