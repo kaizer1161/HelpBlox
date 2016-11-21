43d2e54fdcd34e3431267f1189ec0253c89c5a92
kaizer <mir.rayan.iub@gmail.com>
2016-11-21 21:17:09 +0600

    Category Activity and layout set

---
+++ app/src/main/java/dsd/informme/com/helpblox/CategoryActivity.java
@@ -0,0 +1,57 @@
+package dsd.informme.com.helpblox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kaizer on 11/21/16.
+ */
+
+public class CategoryActivity  extends AppCompatActivity {
+
+        ArrayList<String> categoryName = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        categoryName.add("Google");
+        categoryName.add("Github");
+        categoryName.add("Instagram");
+        categoryName.add("Facebook");
+        categoryName.add("Flickr");
+        categoryName.add("Pinterest");
+        categoryName.add("Quora");
+        categoryName.add("Twitter");
+        categoryName.add("Vimeo");
+        categoryName.add("WordPress");
+        categoryName.add("Youtube");
+        categoryName.add("Stumbleupon");
+        categoryName.add("SoundCloud");
+        categoryName.add("Reddit");
+        categoryName.add("Blogger");
+
+        CategoryAdapter adapter = new CategoryAdapter(CategoryActivity.this, categoryName);
+        GridView grid = (GridView)findViewById(R.id.category_gridView_id);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(CategoryActivity.this, "You Clicked at " +categoryName, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+}
+
---
+++ app/src/main/java/dsd/informme/com/helpblox/CategoryAdapter.java
@@ -0,0 +1,62 @@
+package dsd.informme.com.helpblox;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by kaizer on 11/21/16.
+ */
+public class CategoryAdapter extends BaseAdapter{
+
+    ArrayList<String> nameOfCategory;
+    Context context;
+
+    public CategoryAdapter(Context context, ArrayList<String> nameOfCategory){
+
+        this.context = context;
+        this.nameOfCategory = nameOfCategory;
+
+    }
+
+    @Override
+    public int getCount() {
+        return nameOfCategory.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            grid = inflater.inflate(R.layout.activity_category_grid, null);
+            CheckBox checkBox = (CheckBox) grid.findViewById(R.id.checkbox_id);
+            checkBox.setText(nameOfCategory.get(position));
+        } else {
+            grid = convertView;
+        }
+
+        return grid;
+    }
+}
