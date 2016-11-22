e75a3bca045df731e94c53982001a4d047aa659e
kaizer <mir.rayan.iub@gmail.com>
2016-11-22 17:18:58 +0600

    userID and passwords are saved in a variable

--- app/src/main/java/dsd/informme/com/helpblox/LoginActivity.java
+++ app/src/main/java/dsd/informme/com/helpblox/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by kaizer on 11/21/16.
@@ -18,20 +19,28 @@ public class LoginActivity extends AppCompatActivity {
     */
 
     Button loginBtn, newAccountBtn;
-    String userName, Password;
+    String userId, password;
+    EditText idEditText, passwordEditText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Attaching all the widgets to their corresponding view id.
         loginBtn = (Button) findViewById(R.id.loginBtn_id);
         newAccountBtn = (Button) findViewById(R.id.newAccountBtn_id);
+        idEditText = (EditText) findViewById(R.id.usernameEditText_id);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText_id);
 
         //click listener for login Button. clicking Login Button makes intent for MainActivity.
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                userId = String.valueOf(idEditText.getText());
+                password = String.valueOf(passwordEditText.getText());
+
                 Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(loginIntent);
             }
