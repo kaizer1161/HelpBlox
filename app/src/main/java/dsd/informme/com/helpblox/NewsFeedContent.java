6b0ebbf89b9cbb12cd1e2c79432532f73edae00e
kaizer <mir.rayan.iub@gmail.com>
2016-11-25 15:57:12 +0600

    NewsFeed activity created with Navigation Drawer.

---
+++ app/src/main/java/dsd/informme/com/helpblox/NewsFeedContent.java
@@ -0,0 +1,47 @@
+package dsd.informme.com.helpblox;
+
+/**
+ * Created by kaizer on 11/24/16.
+ */
+
+public class NewsFeedContent {
+
+    //All private variables of this class is declared here.
+    private int imageResourceId;
+    private String userName, timeAndDate, newsContent;
+
+    public NewsFeedContent(int imageResourceId, String userName, String timeAndDate, String newsContent){
+
+        this.imageResourceId = imageResourceId;
+        this.userName = userName;
+        this.timeAndDate = timeAndDate;
+        this.newsContent = newsContent;
+
+
+    }
+
+    public int getImageRecourseId(){
+
+        return imageResourceId;
+
+    }
+
+    public String getUserName(){
+
+        return userName;
+
+    }
+
+    public String getTimeAndDate(){
+
+        return timeAndDate;
+
+    }
+
+    public String getNewsContent(){
+
+        return newsContent;
+
+    }
+
+}
