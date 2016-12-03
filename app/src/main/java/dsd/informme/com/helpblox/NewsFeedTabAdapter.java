cd7562e8c13b3397349542a6aa3a43567bfdf8f8
kaizer <mir.rayan.iub@gmail.com>
2016-12-03 16:47:30 +0600

    new class NewsFeed and NewsFeedTabAdapter is created to implement tabs

---
+++ app/src/main/java/dsd/informme/com/helpblox/NewsFeedTabAdapter.java
@@ -0,0 +1,54 @@
+package dsd.informme.com.helpblox;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by kaizer on 12/3/16.
+ */
+
+public class NewsFeedTabAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+
+    public NewsFeedTabAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0){
+            return new NewsFeed();
+        }
+        else if (position == 1){
+            return new NewsFeed();
+        }
+        else if (position == 2) {
+            return new NewsFeed();
+        }
+        else {
+            return new NewsFeed();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return context.getString(R.string.news);
+        }
+        else if (position == 1) {
+            return context.getString(R.string.trending);
+        }
+        else {
+            return context.getString(R.string.profil);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
