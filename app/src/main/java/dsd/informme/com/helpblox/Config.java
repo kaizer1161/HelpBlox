7a05d9cc9db99b7005c7657f17a9a26e395f98a3
xubair shoumik <1330082@iub.edu.bd>
2016-12-31 01:35:57 +0600

    Config.java
    Configuration file for name fetch from DB

---
+++ app/src/main/java/dsd/informme/com/helpblox/Config.java
@@ -0,0 +1,17 @@
+package dsd.informme.com.helpblox;
+
+/**
+ * Created by xubi on 30-Dec-16.
+ */
+
+public class Config {
+    public static final String DATA_URL = "http://test.artefactplus.com/user.php?email=";
+    public static final String KEY_NAME = "name";
+    //public static final String KEY_ID = "id";
+    //public static final String KEY_EMAIL = "email";
+    //public static final String KEY_PASSWORD = "password";
+    //public static final String KEY_BLOODGROUP = "blood_group";
+    //public static final String KEY_PROFILEPIC = "profile_pic";
+
+    public static final String JSON_ARRAY = "result";
+}
