c2dbaaddb64fffbced001989c631eea7b3f2b3c4
kaizer <mir.rayan1@gmail.com>
2017-01-01 13:50:28 +0600

    NewsFeed fetch from DB and minor UI update

--- app/src/main/java/dsd/informme/com/helpblox/LoginActivity.java
+++ app/src/main/java/dsd/informme/com/helpblox/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -40,7 +39,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     EditText idEditText, passwordEditText;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //see if the user is logged in shared memory
